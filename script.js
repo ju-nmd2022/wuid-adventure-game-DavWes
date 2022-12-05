7ef50f7d66a89098d1932b2fb577c200a53d0480
DavWes <weda18pc@student.ju.se>
2022-12-06 00:27:57 +0100

    commit

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-let isRingPicked = false;
-let isSamPicked = false;
-let isStingPicked = false;
+// let isRingPicked = false;
+// let isSamPicked = false;
+// let isStingPicked = false;
+
+let ring = document.getElementById("thering");
+let indexroom = document.getElementById("indexroom");
 
 // INDEX
-window.addEventListener("load", function () {});
-if (window.localStorage.isRingPicked === true) {
-  document.getElementById("thering").style.display = "block";
-  const indexroom = document.getElementById("indexroom");
-  indexroom.src = "images/thering.png";
-}
-
-const thering = document.getElementById("thering");
-
 indexroom.addEventListener("click", function () {
-  document.getElementById("thepick").style.display = "block";
-  indexroom.src = "images/thering.png";
-  window.localStorage.isRingPicked = true;
-  console.log("hejhej");
+  ring.style.visibility = "visible";
+  sessionStorage.setItem("saveRing", "itemClick");
 });
-
-// else {
-//   // alert("You forgot to take the ring");
-// }
