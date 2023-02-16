feff46daec76015a42084f2d29da7d5f69d9fbac
DavWes <weda18pc@student.ju.se>
2023-02-17 00:49:42 +0100

    commit

--- mountdoom.js
+++ mountdoom.js
@@ -6,6 +6,25 @@ let samimage = document.getElementById("samimage");
 let thesam = document.getElementById("thesam");
 let bilbo = document.getElementById("bilbo");
 let thesting = document.getElementById("thesting");
+// let fightbutton = document.getElementById("fightbutton");
+// let fightbuttongollum = document.getElementById("fightbuttongollum");
+
+const hpCount = document.getElementById("number");
+const button = document.querySelector("button");
+
+let x = 10;
+
+hpCount.innerHTML = x;
+
+button.addEventListener("click", function () {
+  function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  const randomNumResult = randomNum(1, 2);
+
+  x = x - randomNumResult;
+  hpCount.innerHTML = x;
+});
 
 if (sessionStorage.getItem("savethesam") != null) {
   thesam.style.visibility = "visible";
--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let thesam = document.getElementById("thesam");
 let bilbo = document.getElementById("bilbo");
 let thesting = document.getElementById("thesting");
 
+// let ringbutton = document.getElementById("ringbutton");
+
 indexroom.addEventListener("click", function () {
   inventory.push("indexroom");
   thering.style.visibility = "visible";
@@ -21,6 +23,15 @@ if (sessionStorage.getItem("savethesam") != null) {
 if (sessionStorage.getItem("savethering") != null) {
   thering.style.visibility = "visible";
 }
+
+// ringbutton.addEventListener("click", function () {
+//   if (sessionStorage.getItem("savethering") === null) {
+//     alert("You need to take the ring!");
+//     location.href = "index.html";
+//   } else {
+//     location.href = "companion.html";
+//   }
+// });
 
 function startAgain() {
   sessionStorage.removeItem("savethering", "itemClick");
